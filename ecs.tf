dc2fe566d93c61b68b347266f12ba2e0e9d72290
KinoshitaRiku <80682674+KinoshitaRiku@users.noreply.github.com>
2022-05-08 22:51:47 +0900

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -11,20 +11,20 @@ resource "aws_ecs_cluster" "chatapp" {
   name = "chatapp-cluster"
 }
 
-resource "aws_ecs_service" "chatapp" {
-  launch_type = "FARGATE"
-  task_definition = "chatapp-test"
-  cluster = aws_ecs_cluster.chatapp.id
-  name = "chatapp-service"
-  desired_count = 1
+# resource "aws_ecs_service" "chatapp" {
+#   launch_type = "FARGATE"
+#   task_definition = "chatapp-test"
+#   cluster = aws_ecs_cluster.chatapp.id
+#   name = "chatapp-service"
+#   desired_count = 1
 
-  network_configuration {
-    assign_public_ip = true
-    security_groups  = [
-      aws_security_group.chatapp.id
-    ]
-    subnets = [
-      aws_subnet.public_1a.id
-    ]  
-  }
-}
+#   network_configuration {
+#     assign_public_ip = true
+#     security_groups  = [
+#       aws_security_group.chatapp.id
+#     ]
+#     subnets = [
+#       aws_subnet.public_1a.id
+#     ]  
+#   }
+# }
